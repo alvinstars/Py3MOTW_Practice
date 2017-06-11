c72ff2d4e7dfa0429a3e15a61e34db0d5346e38a
Bin Yang <alvinstars@gmail.com>
2017-06-11 12:12:27 -0400

    abc

---
+++ Language_Tools/abc_subclass.py
@@ -0,0 +1,19 @@
+# abc_subclass.py
+import abc
+from abc_base import PluginBase
+
+
+class SubclassImplementation(PluginBase):
+
+    def load(self, input):
+        return input.read()
+
+    def save(self, output, data):
+        return output.write(data)
+
+
+if __name__ == '__main__':
+    print('Subclass:', issubclass(SubclassImplementation,
+                                  PluginBase))
+    print('Instance:', isinstance(SubclassImplementation(),
+                                  PluginBase))
